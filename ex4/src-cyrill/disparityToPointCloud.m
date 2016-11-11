2ff4c88d59b34273643ea8d8582f8449ceea7bbd
cyrillhalter <cyrill.halter@me.com>
2016-11-11 14:26:25 +0100

    xyz

--- ex4/src-cyrill/disparityToPointCloud.m
+++ ex4/src-cyrill/disparityToPointCloud.m
@@ -5,5 +5,40 @@ function [points, intensities] = disparityToPointCloud(...
 % for pixels of left_img which have a valid disparity estimate! The i-th
 % intensity should correspond to the i-th point.
 
+points = zeros(3,1);
+intensities = zeros(1);
+
+K_inv = inv(K);
+
+n = 1;
+
+for row = 1:size(disp_img,1)
+    
+   for col = 1:size(disp_img,2)
+       
+       disparity = disp_img(row,col);
+       
+       if disparity > 0
+           
+           A =  [K_inv * [col;row;1] -K_inv * [col-disparity;row;1]];
+           
+           lambda = A \ [baseline; 0; 0];
+           
+           P = lambda(1) * K_inv * [col; row; 1];
+           
+           points(:,n) = P;
+           intensities(n) = left_img(row,col);
+           
+           n = n+1;
+          
+           
+       end
+       
+       
+   end
+    
+    
+end
+
 end
 
