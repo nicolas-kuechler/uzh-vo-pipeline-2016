bd333a5c3238458b3089784c897d05a533e22b7c
Nicolas Küchler <Nicolas Kchler>
2016-11-11 13:48:29 +0100

    ex4 sol and ex 5

--- ex4/src-nicolas/getDisparity.m
+++ ex4/src-nicolas/getDisparity.m
@@ -17,7 +17,7 @@ last_col = size(left_img, 2) - patch_radius;
 disp_img = zeros(last_row-first_row+1, last_col-first_col+1);
 
 indices = (1:last_col-first_col + 1);
-
+count = 0;
 for row = first_row : last_row
     left_patches = zeros(patch_size, last_col-first_col + 1);
     right_patches = zeros(patch_size, last_col-first_col + 1);
@@ -32,23 +32,47 @@ for row = first_row : last_row
     end
     
     
+    D = pdist2(right_patches', left_patches','squaredeuclidean');
+    [M,I] = min(D);
     
-    [D,I] = pdist2(right_patches', left_patches','squaredeuclidean', 'Smallest', 3);
+    n = size(D,2);
+    Int_I = zeros(1,n);
     
-    disp = indices-I(1,:);
+    for i = 1:n
+        %1st Check D < 1.5 M
+        minimum_match = 1.5 * M(i);
+        sum_sat_cond = sum(D(:,i)<=minimum_match);
+        
+        if sum_sat_cond <=2 & i-I(i) > min_disp & i-I(i) < max_disp
+            count = count + 1;
+            left_i = i-1;
+            left_i(left_i<1)=1;
+            right_i = i+1;
+            right_i(right_i>n)=n;
+           
+            x = [left_i i right_i];
+            y = [D(I(i),left_i) M(i) D(I(i),right_i)];
+            
+            p = polyfit(x,y,2);
+            d1p = polyder(p);
+            Int_I(i) = roots(d1p);
+        end      
+    end
+    
+    disp = indices-Int_I;
+    disp(Int_I==0 | disp<=min_disp | disp >= max_disp)=0;
+    
+   
+    %[D,I] = pdist2(right_patches', left_patches','squaredeuclidean', 'Smallest', 3);
+    
+   
     
     %Filter 1: Reject if 3 or more matches with d <= 1.5 * min   
-    minimum_matches = 1.5 * D(1,:);
-    occ_sat_cond = sum(D<=minimum_matches);
-    disp(occ_sat_cond>2 | disp>=max_disp | disp<=min_disp) = 0;
+    %minimum_matches = 1.5 * D(1,:);
+    %occ_sat_cond = sum(D<=minimum_matches);
+    %disp(occ_sat_cond>2 | disp>=max_disp | disp<=min_disp) = 0;
     
     % Part 4
-    for i=1:size(disp,1)
-        
-        
-        pdist2(
-        
-    end
     
     %
     
