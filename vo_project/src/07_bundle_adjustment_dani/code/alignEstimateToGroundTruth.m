7d474d2d3b39b2c8354133d6e8375b008c773a2b
Daniel Gehrig <danielgehrig@Daniels-MacBook-Pro-3.local>
2016-12-24 12:59:48 +0100

    alignEstimateToGroundTruth: taken input p_W_GT = ground truth (3 x N) and p_W_estimate = estimated poses (3 x N) and returns the alined poses and error

--- vo_project/src/07_bundle_adjustment_dani/code/alignEstimateToGroundTruth.m
+++ vo_project/src/07_bundle_adjustment_dani/code/alignEstimateToGroundTruth.m
@@ -1,11 +1,11 @@
-function p_W_estimate_aligned = alignEstimateToGroundTruth(...
+function [p_W_estimate_aligned, error] = alignEstimateToGroundTruth(...
     p_W_GT, p_W_estimate)
 
 x0 = [0, 0, 0, 0, 0, 0, 1]';
 
 % determine solution
 error_function = @(x) alignmentError(x, p_W_GT, p_W_estimate);
-x = lsqnonlin(error_function, x0);
+[x, error] = lsqnonlin(error_function, x0);
 
 T = twist2HomogMatrix(x(1:6, 1));
 R = T(1:3, 1:3);
