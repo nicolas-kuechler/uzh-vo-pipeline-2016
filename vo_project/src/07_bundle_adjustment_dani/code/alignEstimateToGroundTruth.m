8165ff303a525495ae4f0936831121780d83296c
Daniel Gehrig <danielgehrig@Daniels-MacBook-Pro-3.local>
2016-12-24 12:56:40 +0100

    added function to align the estimated poses to the ground truth

---
+++ vo_project/src/07_bundle_adjustment_dani/code/alignEstimateToGroundTruth.m
@@ -0,0 +1,17 @@
+function p_W_estimate_aligned = alignEstimateToGroundTruth(...
+    p_W_GT, p_W_estimate)
+
+x0 = [0, 0, 0, 0, 0, 0, 1]';
+
+% determine solution
+error_function = @(x) alignmentError(x, p_W_GT, p_W_estimate);
+x = lsqnonlin(error_function, x0);
+
+T = twist2HomogMatrix(x(1:6, 1));
+R = T(1:3, 1:3);
+t = T(1:3, 4);
+s = x(7);
+
+p_W_estimate_aligned = s * R * p_W_estimate + t;
+end
+
