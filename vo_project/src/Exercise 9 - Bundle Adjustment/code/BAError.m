dad2591a81a81b3fd02591cdb234c77909070ced
Daniel Gehrig <danielgehrig@daniels-macbook-pro-3.home>
2016-12-30 01:24:53 +0100

    improved BA error function for speed

--- vo_project/src/Exercise 9 - Bundle Adjustment/code/BAError.m
+++ vo_project/src/Exercise 9 - Bundle Adjustment/code/BAError.m
@@ -1,24 +1,25 @@
-function e = BAErrorFunction( hidden_state, observations, K)
-%BAERRORFUNCTION Summary of this function goes here
-%   Detailed explanation goes here
+function e = BAErrorFunction( hidden_state, observations, K, y_dim)
+%BAERRORFUNCTION error function that is called where running runBA
 n = observations(1);
 m = observations(2);
 
 counter = 3;
-Y = [];
-fx = [];
+kp_counter = 1;
+Y = zeros(1,y_dim);
+fx = zeros(1, y_dim);
+
 % separate hidden_state
 land_marks = reshape(hidden_state(6 * n + 1 : end), 3, m);
 taus = reshape(hidden_state(1: 6 * n), 6, n);
 
 for i = 1 : n
-    % get homography for frame i
-    homography = twist2HomogMatrix(taus(:, i)); 
+    % get homography for frame i (inv because inverse is saved in the
+    % hidden state)
+    homography = inv(twist2HomogMatrix(taus(:, i))); 
     
     % get ith observation
     k_i = observations(counter);
     observation_i = observations(counter + 1: counter + 3 * k_i);
-    counter = counter + 3 * k_i + 1;
     
     % extract correspondence and kp
     p_i = observation_i(1 : 2 * k_i);
@@ -30,8 +31,12 @@ for i = 1 : n
     % reproject landmarks
     fx_i = reprojectPoints(lm, homography(1:3, :), K);
     
-    fx = [fx, fx_i(:)'];
-    Y = [Y, p_i];
+    fx(kp_counter : kp_counter + 2 * k_i - 1) = fx_i(:)';
+    Y(kp_counter : kp_counter + 2 * k_i - 1) = p_i;
+    
+    % increment counter
+    kp_counter = kp_counter + 2 * k_i;
+    counter = counter + 3 * k_i + 1;
 end
 
 e = Y - fx;
