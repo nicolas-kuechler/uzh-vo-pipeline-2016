cd622f62031f7c5b8d739aa5f7c600140970c782
Daniel Gehrig <danielgehrig@daniels-macbook-pro-3.home>
2016-12-30 15:19:22 +0100

    deleted old code. added flag in triangulate to check if enough key points are matched. If this is not the case outliers are not removed.

--- vo_project/src/tryTriangulate.m
+++ vo_project/src/tryTriangulate.m
@@ -1,5 +1,5 @@
 function [cloud, matched_kp, remain] = ...
-    tryTriangulate(candidates_current, candidates_start, candidates_start_pose, T, K, params)
+    tryTriangulate(candidates_current, candidates_start, candidates_start_pose, T, K, flag, params)
 % TRYTRIANGULATE function that takes candidate keypoints (candidate_k) and
 % key points at the start of the track (kp_track_start) and triangulates
 % new 3D points if their bearing angle in between is large enough.
@@ -12,6 +12,8 @@ function [cloud, matched_kp, remain] = ...
 %                        the candidate key points were first detected.
 %         T: (3 x 4) camera pose from the current frame
 %         K: (3 x 3) camera calibration matrix.
+%         params: parameter struct containing parameters to execute
+%                 triangulation
 
 num_kp = size(candidates_current, 2);
 
@@ -35,7 +37,6 @@ angles = 180/pi * acos(sum(u_norm .* v_norm));
 tri = angles > params.triangulation_angle_threshold;
 
 % triangulate all eligible points 
-%% MATLAB Comparison
 cameraParams = cameraParameters('IntrinsicMatrix',K');
 new_3d_pt = zeros(3, sum(tri));
 repr_Errors = zeros(1, sum(tri));
@@ -58,68 +59,16 @@ end
 % remove those with high repr error and negative z
 z_component = T(3, 1 : 3) * new_3d_pt + T(3, 4);
 z_mask = z_component > 0;
-r_mask = repr_Errors < params.triangulate_max_repr_error;
+if flag
+    r_mask = repr_Errors < params.triangulate_max_repr_error;
+else
+    r_mask = repr_Errors > 0;
+end
 
 % extract all points and keypoints and construct remain vector
 cloud = new_3d_pt(:, z_mask & r_mask);
 matched_kp = candidates_current(:, triangulation_indices(z_mask & r_mask));
 
 remain = ~tri;
-
-% 
-% for i = 1 : num_kp
-%     v = candidate_kp_bearings(:, i);
-%     T_i = reshape(candidates_start_pose(:,i), 3, 4);
-%     
-%     % transform track start bearing vector in current camera frame
-%     u = T(:,1:3) * T_i(:, 1:3)' * candidate_start_bearings(:, i);
-%     
-%     % calculate the angle between bearing vectors
-%     Theta = 180 / pi * acos(dot(u/norm(u), v/norm(v)));
-%     
-%     if Theta > params.triangulation_angle_threshold
-%         % if the angle is big enough triangulate a 3d point between the track
-%         % start key point and the current candidate key point
-%         %new_3d_pt = linearTriangulation(candidates_start(:, i), ...
-%         %    candidates_current(:, i), K * T_i ,K * T);
-%         
-%         %% MATLAB Comparison
-%         cameraParams = cameraParameters('IntrinsicMatrix',K');
-%         orientation1 = T_i(:,1:3)';
-%         location1 = T_i(:,4);
-%         cameraMatrix1 = cameraMatrix(cameraParams,orientation1,location1);
-%         
-%         orientation2 = T(:,1:3)';
-%         location2 = T(:,4);
-%         cameraMatrix2 = cameraMatrix(cameraParams,orientation2,location2);
-%         
-%         [worldPoint, reprojectionError]= triangulate(candidates_current(:, i)',candidates_start(:, i)',cameraMatrix2,cameraMatrix1);
-%         new_3d_pt = worldPoint';
-%         %% 
-%         
-%         % transform into current camera frame
-%         new_3d_pt_camera_frame = T(:, 1:3) * new_3d_pt + T(:, 4);
-%         
-%         % reject new 3d point if it lies behind the camera (z < 0)
-%         if reprojectionError(1) < params.triangulate_max_repr_error && new_3d_pt_camera_frame(3) > 0
-%             cloud = [cloud, new_3d_pt];
-%             matched_kp = [matched_kp, candidates_current(:, i)];
-%         end
-%         % current keypoint is no longer tracked
-%         remain(i) = false;    
-%     end
-%     
-% end
-debug = false;
-%% DEBUG
-if debug
-    figure(4)
-    imshow(new_image);
-    hold on;
-    % plot track start of each candidate keypoints starts
-    plot(candidates_start(1,:), candidates_start(2,:), 'go', 'Linewidth', 2);
-    % plot track start of each candidate keypoints
-    plot(candidates_current(1,:), candidates_current(2,:), 'rx', 'Linewidth', 2);
-end
 
 end
