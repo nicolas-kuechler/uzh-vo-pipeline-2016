147e332f2031bf7eb237ffb05e62f52fe3a72443
Daniel Gehrig <danielgehrig@public-docking-lee-1044.ethz.ch>
2017-01-04 16:17:59 +0100

    made some adjustments to parameters

--- vo_project/src/propagateState.m
+++ vo_project/src/propagateState.m
@@ -2,7 +2,7 @@ function [next_kp, point_validity] = propagateState(kp, prev_img, next_img)
 %PROPAGATESTATE Summary of this function goes here
 %   Detailed explanation goes here
 % create pointTracker
-pointTracker = vision.PointTracker;
+pointTracker = vision.PointTracker('MaxBidirectionalError', 2.1, 'BlockSize', [13,13]);
 
 % initialize point tracker
 initialize(pointTracker, kp', prev_img);
